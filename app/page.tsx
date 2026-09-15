c440ce52296f438a9fa7878348275a59a64fc1bf
ajaykumarpandey8076-del <ajaykumarpandey8076@gmail.com>
2026-09-15 20:26:00 +0530

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: "40px", fontFamily: "sans-serif", textAlign: "center" }}>
-      <h1>Form Saathi AI - Pi Mainnet</h1>
-      <p>Welcome to our official Pi Network application. Everything is running successfully!</p>
-    </main>
-  );
-}
